d473e257e4739f652a86a2e7b4a4938e4c24b0c7
hakur <yuxing951@gmail.com>
2021-09-02 16:37:36 +0800

    update xtrabackup

--- xtrabackup/function.sh
+++ xtrabackup/function.sh
@@ -12,6 +12,8 @@ MAX_SERVER_ID=${MAX_SERVER_ID:-2}
 FULL_BACKUP_ENABLED=${FULL_BACKUP_ENABLED:-"true"}
 FULL_BACKUP_DIR=${FULL_BACKUP_DIR:-/data/full}
 
+BACKUP_MYSQL_DATA_DIR=${BACKUP_MYSQL_DATA_DIR:-/var/lib/mysql}
+
 INCREMENT_BACKUP_ENABLED=${INCREMENT_BACKUP_ENABLED:-"true"}
 INCREMENT_BACKUP_DIR=${INCREMENT_BACKUP_DIR:-/data/increment}
 
@@ -62,7 +64,7 @@ function fullBackup() {
     echolog "start full backup, master server is ${server}"
     if [ "$ENABLE_S3_UPLOAD" == "true" ];then
         mc alias set auth $S3_ENDPOINT $S3_ACCESS_KEY $S3_SECURITY_KEY
-        xtrabackup --default-file=$CNF_FILE --host=$server --user=$MYSQL_USER --password=$MYSQL_PASSWORD --backup --port=$MYSQL_CONN_PORT --datadir=/data/mysql --stream=tar | gzip | mc pipe auth/$S3_BUCKET/$S3_OBJECT_DIR/$(basename $targetDir)_from-${ATTACHED_MYSQL_HOST}.tar.gz
+        xtrabackup --default-file=$CNF_FILE --host=$server --user=$MYSQL_USER --password=$MYSQL_PASSWORD --backup --port=$MYSQL_CONN_PORT --datadir=$BACKUP_MYSQL_DATA_DIR --stream=tar | gzip | mc pipe auth/$S3_BUCKET/$S3_OBJECT_DIR/$(basename $targetDir)_from-${ATTACHED_MYSQL_HOST}.tar.gz # bug,无法检测xtrabackup的exit code
     else
         if [ "$targetDir" == "" ];then
             echolog "targetDir argument is empty"
@@ -77,7 +79,7 @@ function fullBackup() {
             mkdir -p $targetDir
         fi
 
-        xtrabackup --default-file=$CNF_FILE --host=$server --user=$MYSQL_USER --password=$MYSQL_PASSWORD --backup --port=$MYSQL_CONN_PORT --target-dir=$targetDir --datadir=/data/mysql
+        xtrabackup --default-file=$CNF_FILE --host=$server --user=$MYSQL_USER --password=$MYSQL_PASSWORD --backup --port=$MYSQL_CONN_PORT --target-dir=$targetDir --datadir=$BACKUP_MYSQL_DATA_DIR
     fi
 
     local code=$?
@@ -85,7 +87,32 @@ function fullBackup() {
         rm -rf $targetDir
     fi
 
+    backupBinlog $targetDir
+    local code=$?
+    if [ $code != 0 ];then
+        rm -rf $targetDir
+    fi
+
     return $code
+}
+
+# backupBinlog /data/backup/$(date '+%Y-%m-%d_%H-%M')
+function backupBinlog() {
+    local targetDir=$1
+    local currentDir=$(pwd)
+    local files=""
+
+    mc alias set auth $S3_ENDPOINT $S3_ACCESS_KEY $S3_SECURITY_KEY
+
+    while read line;do
+        if [ -f "$BACKUP_MYSQL_DATA_DIR/$line" ];then
+            files=$files" "$line
+        fi
+    done < $BACKUP_MYSQL_DATA_DIR/bin.index
+    
+    cd $BACKUP_MYSQL_DATA_DIR
+    tar --to-stdout -cz $files | mc pipe auth/$S3_BUCKET/$S3_OBJECT_DIR/$(basename $targetDir)_from-${ATTACHED_MYSQL_HOST}.tar.gz.binlog
+    cd $currentDir
 }
 
 # function incrementBackup() {
@@ -156,7 +183,9 @@ function restore() {
         return $code
     fi
 
-    mv $RECOVER_MYSQL_DATA_DIR/* $oldDataDir
+    cp -r $RECOVER_MYSQL_DATA_DIR/* $oldDataDir
+
+    rm -rf $RECOVER_MYSQL_DATA_DIR/*
 
     echolog "Run command : xtrabackup --default-file=$CNF_FILE --copy-back --target-dir=$recoverDataDir --datadir=$RECOVER_MYSQL_DATA_DIR"
 
@@ -164,9 +193,21 @@ function restore() {
     code=$?
     rm -rf $RECOVER_TMP_DIR/recover
     if [ $code != 0 ];then
-        mv $oldDataDir/* $RECOVER_MYSQL_DATA_DIR
+        cp -r $oldDataDir/* $RECOVER_MYSQL_DATA_DIR
         return $code
     fi
+
+    cd $RECOVER_MYSQL_DATA_DIR
+    mc cat auth/$S3_BUCKET/$S3_FILE.binlog | tar -zxf -
+    code=$?
+    if [ $code != 0 ];then
+        rm -rf $RECOVER_MYSQL_DATA_DIR
+        cp -r $oldDataDir/* $RECOVER_MYSQL_DATA_DIR
+        return $code
+    fi
+
+    mc cat auth/$S3_BUCKET/$S3_FILE.binlog | tar -ztvf - | awk '{print $6}' >> bin.index
+
     useradd mysql
     chmod -R 777 $RECOVER_MYSQL_DATA_DIR
     chown -R mysql:mysql $RECOVER_MYSQL_DATA_DIR
